77a974559101d87b3224dec8640eb6f1d2fc83b4
Hotelsoft User <user1@IL1046.local>
2019-07-26 20:58:48 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {FormControl, InputLabel, Select, MenuItem} from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    city: [],
+    selectedCity: ""
+  }
+
+  handleChange = async(e) => {
+    e.preventDefault();
+    console.log(e.target);
+    const city = e.target.value;
+    const api_call = await fetch(`https://vast-shore-74260.herokuapp.com/banks?city=MUMBAI`);
+    const data = await api_call.json();
+    console.log("data is:", data);
+    console.log("city", e.target.value);
+
+    this.setState({
+      selectedCity: e.target.value
+    })
+  }
+  render() {
+    return(
+      <div>
+         <form autoComplete="off">
+          <FormControl>
+            <InputLabel htmlFor="select-city">Select City</InputLabel>
+            <Select
+              value={this.state.city}
+              onChange={this.handleChange}
+              inputProps={{
+                name: 'select-city',
+                id: 'select-city',
+              }}
+              fullWidth={true}
+            >
+                <MenuItem>{this.state.city}</MenuItem>
+            </Select>
+          </FormControl> 
+        </form> 
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/test.js
@@ -0,0 +1,18 @@
+{/* <form autoComplete="off">
+          <FormControl>
+            <InputLabel htmlFor="select-city">Select City</InputLabel>
+            <Select
+              value={this.state.city}
+              onChange={this.handleChange}
+              inputProps={{
+                name: 'select-city',
+                id: 'select-city',
+              }}
+              fullWidth={true}
+            >
+              city.map(city => {
+                <MenuItem key={data.city}>{this.state.city}</MenuItem>
+              })
+            </Select>
+          </FormControl> */}
+        </form>  
